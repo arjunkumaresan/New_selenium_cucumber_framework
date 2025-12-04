5daee5bd12d61346f6799667b16a3839ecc9dd56
arjunkumaresan <arjunkumaresan@gmail.com>
2025-12-04 17:51:20 +0530

    Fix Driverfactory method name in Hooks

--- src/test/java/Hooks/Hooks.java
+++ src/test/java/Hooks/Hooks.java
@@ -1,29 +1,39 @@
-package stepdefinitions;
+package Hooks;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import utilities.ConfigReader;
 import utilities.Driverfactory;
 
 import java.time.Duration;
+import java.util.Properties;
 
 public class Hooks {
-    WebDriver driver;
+    public static WebDriver driver;
+
 
     @Before
     public void setup()
     {
-        Driverfactory.Initializebrowser("chrome");
-        driver = Driverfactory.getdriver();
-        driver.manage().deleteAllCookies();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("https://tutorialsninja.com/demo/");
+        Properties prop = new ConfigReader().initializeProperties();
+        driver = Driverfactory.Initializebrowser(prop.getProperty("browser"));
+        driver.get(prop.getProperty("url"));
+
     }
 
     @After
-    public void teardown()
+    public void teardown(Scenario scenario)
     {
+        String scenarioname = scenario.getName().replaceAll(" "," ");
+        if(scenario.isFailed())
+        {
+            byte[] screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png",scenarioname);
+        }
         driver.quit();
     }
 }
