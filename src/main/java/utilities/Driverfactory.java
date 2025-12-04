c477374ffb72e636652615e019f71f783a8ebf1c
arjunkumaresan <arjunkumaresan@gmail.com>
2025-12-04 17:31:45 +0530

    Add Driverfactory class for Selenium

--- src/main/java/utilities/Driverfactory.java
+++ src/main/java/utilities/Driverfactory.java
@@ -1,4 +1,4 @@
-package Factory;
+package utilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
@@ -7,16 +7,17 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
+import java.time.Duration;
+import java.util.Properties;
+
 public class Driverfactory {
 
-    static WebDriver driver;
-    public static void Initializebrowser(String browsername)
+    static WebDriver driver = null;
+    public static WebDriver Initializebrowser(String browsername)
     {
-
 
         if(browsername.equals("chrome"))
         {
-            WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver();
         }
         else if(browsername.equals("firefox"))
@@ -31,11 +32,14 @@ public class Driverfactory {
         {
             driver = new SafariDriver();
         }
+        driver.manage().deleteAllCookies();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(Common_utils.PAGE_LOAD_TIME));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Common_utils.IMPLICIT_WAIT_TIME));
 
-
-
+        return driver;
     }
-    public static WebDriver getdriver()
+    public static WebDriver getDriver()
     {
         return driver;
     }
